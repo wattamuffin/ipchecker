691e058767690d804a15b07e551140d952f7e7c2
wattamuffin <turbo21official@gmail.com>
2026-05-14 00:36:58 +0300

    Removed IPv6 support and changed output

--- ipchecker.sh
+++ ipchecker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION="2.0"
+VERSION="2.1"
 
 # Colors
 RED="\033[0;31m"
@@ -9,15 +9,8 @@ GRAY="\033[0;37m"
 WHITE="\033[97m"
 NOCOLOR="\033[0m"
 
-# Sign
-echo -e ""
-echo -e "${GRAY}ipchecker - a simple utility for checking server IP info"
-echo -e "${GRAY}please star the repo on github. thanks"
-echo -e "${GRAY}script version: ${WHITE}${VERSION}"
-
 # Grab info
 ipv4=$(curl -s -4 --max-time 5 ifconfig.me 2>/dev/null)
-ipv6=$(curl -s -6 --max-time 5 ifconfig.me 2>/dev/null)
 data=$(curl -s ipinfo.io)
 
 country=$(echo "$data" | jq -r '.country')
@@ -25,26 +18,10 @@ city=$(echo "$data" | jq -r '.city')
 timezone=$(echo "$data" | jq -r '.timezone')
 asn=$(echo "$data" | jq -r '.org')
 
-# IPv4
-echo -e ""
-if [[ -n "$ipv4" ]]; then
-    echo -e "${GRAY}IPv4: ${WHITE}$ipv4 ${GRAY}| ${GREEN}Supported"
-else
-    echo -e "${GRAY}IPv4: ${RED}Unsupported"
-fi
-
-# IPv6
-if [[ -n "$ipv6" ]]; then
-    echo -e "${GRAY}IPv6: ${WHITE}$ipv6 ${GRAY}| ${GREEN}Supported"
-else
-    echo -e "${GRAY}IPv6: ${RED}Unsupported"
-fi
-
-
 # Other
-echo -e "${GREEN}██╗██████╗  ██████╗██╗  ██╗██╗  ██╗ ${GRAY}Country: ${WHITE}$country"
-echo -e "${GREEN}██║██╔══██╗██╔════╝██║  ██║██║ ██╔╝ ${GRAY}City: ${WHITE}$city"
-echo -e "${GREEN}██║██████╔╝██║     ███████║█████╔╝ ${GRAY}Timezone: ${WHITE}$timezone"
-echo -e "${GREEN}██║██╔═══╝ ██║     ██╔══██║██╔═██╗ ${GRAY}ASN: ${WHITE}$asn"
-echo -e "${GREEN}██║██║     ╚██████╗██║  ██║██║  ██╗"
-echo -e "${GREEN}╚═╝╚═╝      ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝"
+echo -e "${GREEN}██╗██████╗  ██████╗██╗  ██╗██╗  ██╗      ${GRAY}IPv4: ${NOCOLOR}$ipv4"
+echo -e "${GREEN}██║██╔══██╗██╔════╝██║  ██║██║ ██╔╝      "
+echo -e "${GREEN}██║██████╔╝██║     ███████║█████╔╝       ${GRAY}Country: ${NOCOLOR}$country"
+echo -e "${GREEN}██║██╔═══╝ ██║     ██╔══██║██╔═██╗       ${GRAY}City: ${NOCOLOR}$city"
+echo -e "${GREEN}██║██║     ╚██████╗██║  ██║██║  ██╗      ${GRAY}Timezone: ${NOCOLOR}$timezone"
+echo -e "${GREEN}╚═╝╚═╝      ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝      ${GRAY}ASN: ${NOCOLOR}$asn"
