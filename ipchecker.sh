2755079e6f5b74bcf88c6ae4c397c158adee13ae
wattamuffin <turbo21official@gmail.com>
2026-05-14 00:49:03 +0300

    Update colors again

--- ipchecker.sh
+++ ipchecker.sh
@@ -4,7 +4,7 @@ VERSION="2.1"
 # Colors
 RED="\033[0;31m"
 GREEN="\033[0;92m"
-BLUE="\033[0;32m"
+BLUE="\033[0;36m"
 GRAY="\033[0;37m"
 WHITE="\033[97m"
 NOCOLOR="\033[0m"
@@ -19,9 +19,9 @@ timezone=$(echo "$data" | jq -r '.timezone')
 asn=$(echo "$data" | jq -r '.org')
 
 # Other
-echo -e "${BLUE}██╗██████╗  ██████╗██╗  ██╗██╗  ██╗      \033[36mIPv4: ${NOCOLOR}$ipv4"
+echo -e "${BLUE}██╗██████╗  ██████╗██╗  ██╗██╗  ██╗      IPv4: ${NOCOLOR}$ipv4"
 echo -e "${BLUE}██║██╔══██╗██╔════╝██║  ██║██║ ██╔╝      "
-echo -e "${BLUE}██║██████╔╝██║     ███████║█████╔╝       ${BLUE}Country: ${NOCOLOR}$country"
-echo -e "${BLUE}██║██╔═══╝ ██║     ██╔══██║██╔═██╗       ${BLUE}City: ${NOCOLOR}$city"
-echo -e "${BLUE}██║██║     ╚██████╗██║  ██║██║  ██╗      ${BLUE}Timezone: ${NOCOLOR}$timezone"
-echo -e "${BLUE}╚═╝╚═╝      ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝      ${BLUE}ASN: ${NOCOLOR}$asn"
+echo -e "${BLUE}██║██████╔╝██║     ███████║█████╔╝       Country: ${NOCOLOR}$country"
+echo -e "${BLUE}██║██╔═══╝ ██║     ██╔══██║██╔═██╗       City: ${NOCOLOR}$city"
+echo -e "${BLUE}██║██║     ╚██████╗██║  ██║██║  ██╗      Timezone: ${NOCOLOR}$timezone"
+echo -e "${BLUE}╚═╝╚═╝      ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝      ASN: ${NOCOLOR}$asn"
